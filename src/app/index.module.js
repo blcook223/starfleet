1ba3c3666fccf581a3ada0f91dab10ea07efc5a7
blcook223 <benjamin.l.cook@gmail.com>
2016-04-16 15:11:50 -0500

    removing unused code for reorganization

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -3,6 +3,7 @@
 import { config } from './index.config';
 import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
+import { Henchmen } from '../app/components/henchmen/henchmen.service';
 import { MainController } from './main/main.controller';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
 
@@ -11,5 +12,6 @@ angular.module('batcave', ['ui.router', 'ui.bootstrap', 'toastr'])
   .config(config)
   .config(routerConfig)
   .run(runBlock)
+  .service('Henchmen', Henchmen)
   .controller('MainController', MainController)
   .directive('acmeNavbar', NavbarDirective);
